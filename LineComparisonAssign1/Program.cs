e093ca9793ed80e345ed7d14a7ad071a714e4e3a
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-06 20:12:07 +0530

    Comparison_Of_Lines

--- LineComparisonAssign1/Program.cs
+++ LineComparisonAssign1/Program.cs
@@ -4,59 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the coordinates of the first line (x1, y1, x2, y2):");
-            double[] line1Cordinates = ReadCoordinates();
+            
+                Console.WriteLine("Enter the coordinates of the first line (x1, y1, x2, y2):");
+                double[] line1Cordinates = ReadCoordinates();
 
-            double line1Length = CalculateLineLength(line1Cordinates);
-            Console.WriteLine(line1Length);
+                double line1Length = CalculateLineLength(line1Cordinates);
+                Console.WriteLine(line1Length);
 
-            Console.WriteLine("Enter the coordinates  the second line (x1, y1, x2, y2):");
-            double[] line2Cordinates = ReadCoordinates();
+                Console.WriteLine("Enter the coordinates  the second line (x1, y1, x2, y2):");
+                double[] line2Cordinates = ReadCoordinates();
 
-            double line2Length = CalculateLineLength(line2Cordinates);
-            Console.WriteLine(line2Length);
+                double line2Length = CalculateLineLength(line2Cordinates);
+                Console.WriteLine(line2Length);
 
+                int result = line1Length.CompareTo(line2Length);
+                Console.WriteLine(result);
 
-            int result = line1Length.CompareTo(line2Length);
-            Console.WriteLine(result);
-
-            if (result == 0)
-            {
-                Console.WriteLine("Line 1 is equal to  Line 2");
-            }
-            else
-            {
-                Console.WriteLine("Lines are not equal");
-            }
-
-        }
-
-        static double[] ReadCoordinates()
-        {
-            string input = Console.ReadLine();
-            string[] coordinateStrings = input.Split(',');
-
-            double[] coordinates = new double[4];
-            for (int i = 0; i < 4; i++)
-            {
-                coordinates[i] = double.Parse(coordinateStrings[i]);
+                if (result > 0)
+                {
+                    Console.WriteLine("Line 1 is greater than Line 2");
+                }
+                else if (result < 0)
+                {
+                    Console.WriteLine("Line 2 is greater than Line 1");
+                }
+                else
+                {
+                    Console.WriteLine("Line 1 is equal to Line 2");
+                }
             }
 
-            return coordinates;
+            static double[] ReadCoordinates()
+            {
+                string input = Console.ReadLine();
+                string[] coordinateStrings = input.Split(',');
+
+                double[] coordinates = new double[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    coordinates[i] = double.Parse(coordinateStrings[i]);
+                }
+
+                return coordinates;
+            }
+
+            static double CalculateLineLength(double[] cordinates)
+            {
+                double x1 = cordinates[0];
+                double y1 = cordinates[1];
+                double x2 = cordinates[2];
+                double y2 = cordinates[3];
+
+                double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+                return length;
+            }
+
+
         }
-
-        static double CalculateLineLength(double[] cordinates)
-        {
-            double x1 = cordinates[0];
-            double y1 = cordinates[1];
-            double x2 = cordinates[2];
-            double y2 = cordinates[3];
-
-            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-
-            return length;
-        }
-
-
     }
-}
